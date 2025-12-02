6d051f802fbf9aca26bf84ced0410b2225f4d018
김민태 <mtkim@ai-biz.net>
2025-12-02 10:43:03 +0900

    add del console log api key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -3,8 +3,6 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
-console.log("supabaseAnonKey", supabaseAnonKey);
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   realtime: {
     params: {
